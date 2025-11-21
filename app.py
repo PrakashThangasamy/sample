376af59e4e41dad17a2e17e31763dcf6420c90f7
Prakash <98002602+PrakashThangasamy@users.noreply.github.com>
2025-11-21 15:49:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return jsonify({"message": "Hello CloudEngine Labs - from Python Flask!"})
+    return jsonify({"message": "Hello CBTS - from Python Flask!"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
